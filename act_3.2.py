8fd84f4c4b4308fb621b92d55c34b91b5951148d
suyeon4696 <74674223+suyeon4696@users.noreply.github.com>
2022-04-17 17:59:40 -0500

    Update act_3.2.py

--- act_3.2.py
+++ act_3.2.py
@@ -1,1 +1,27 @@
+import re
 
+print("Nombre del archivo que deseas abrir: ")
+name=input()
+file=open(name)
+
+operators = {'=':'Asignación','*':'Multiplicación','-':'Resta','/':'División','^':'Potencia','+':'Suma','<':'Less than','>':'Greaterthan'}
+operators_key = operators.keys()
+
+dataFlag=False
+a=file.read()
+
+count=0
+program=a.split("\n")
+for line in program:
+    count=count+1
+    print("Line ",count, "\n",line)
+
+    tokens=line.split('')
+    print("Token: ", tokens)
+    print("Line ",count, "Detalles \n")
+    for token in tokens:
+        if token in operators_key:
+            print (token,operators[token])
+
+    dataFlag=False
+    print('-------------------------------------------')
